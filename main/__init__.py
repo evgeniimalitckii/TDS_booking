9295ca8d44ae9bf0578963ad641d00f808a6d838
evgeniimalitckii <evgenii.malitckii@gmail.com>
2021-08-04 13:03:08 +0300

    heroku update

--- main/__init__.py
+++ main/__init__.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-#from flask_script import Manager
-from flask_migrate import Migrate #, MigrateCommand
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -13,18 +12,17 @@ app.config['SECURITY_PASSWORD_SALT'] = 'my_password_salt'
 ####### DATABASE SETUP ########
 ##############################
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 # for localhost
+#basedir = os.path.abspath(os.path.dirname(__file__))
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-# for heroku "tds-booking" project
+
+# for heroku "tds-booking" project, enshure the "postgresql" is used instead just "postgres"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wncgjwthdianhx:e49e7b3fd624473e22d1975d119e83efc9a525b8bbf9769a9401e65bac82c4b1@ec2-3-248-103-75.eu-west-1.compute.amazonaws.com:5432/d79j30n54ibobs'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app=app, db=db)
-
-#manager = Manager(app)
-#manager.add_command('db', MigrateCommand)
 
 ##############################
 ##### REGISTER BLUEPRINT #####
